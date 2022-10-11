9c53b3dba8e56eba2b7372f14152de2c0d0df099
Daniel PC <daniel.barczynski@onet.pl>
2022-10-11 22:04:55 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿int[] numbers = new int[] { 3, 9, 6, 5, 1, 8, 2, 4 };
 int[] lowerNums = new int [] {};
 int[] higherNums = new int [] {};
+int[] sortedNums = new int [] {};
 int length = numbers.Length;
 
 Console.WriteLine("before sorting");
@@ -17,8 +18,11 @@ for (int i = 0; i < length; i++)
     if (numbers[i] < numbers[length - 1]) 
         lowerNums = lowerNums.Append(numbers[i]).ToArray();
     
-    else
+    else if (numbers[i] > numbers[length - 1])
         higherNums = higherNums.Append(numbers[i]).ToArray();
+        
+    else 
+        sortedNums = sortedNums.Append(numbers[i]).ToArray();
 }
 
 for (int i = 0; i < lowerNums.Length; i++)
@@ -31,4 +35,11 @@ Console.WriteLine();
 for (int i = 0; i < higherNums.Length; i++)
 {
     Console.Write(higherNums[i]);
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < sortedNums.Length; i++)
+{
+    Console.Write(sortedNums[i]);
 }
