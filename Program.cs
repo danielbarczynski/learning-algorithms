9850aac37eaa4f3bc5d29064b8e10bfb784bdf54
Daniel PC <daniel.barczynski@onet.pl>
2022-10-11 21:55:16 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = new int[] { 3, 9, 6, 5, 1, 8, 2, 4 };
+int[] lowerNums = new int [] {};
+int[] higherNums = new int [] {};
+int length = numbers.Length;
+
+Console.WriteLine("before sorting");
+
+for (int i = 0; i < length; i++)
+{
+    Console.Write(numbers[i]);
+}
+
+Console.WriteLine("\nafter sorting:");
+
+for (int i = 0; i < length; i++)
+{
+    if (numbers[i] < numbers[length - 1]) 
+        lowerNums = lowerNums.Append(numbers[i]).ToArray();
+    
+    else
+        higherNums = higherNums.Append(numbers[i]).ToArray();
+}
+
+for (int i = 0; i < lowerNums.Length; i++)
+{
+    Console.Write(lowerNums[i]);
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < higherNums.Length; i++)
+{
+    Console.Write(higherNums[i]);
+}
