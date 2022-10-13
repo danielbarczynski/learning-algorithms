e1b609a73f3b9f25f06e1142613a146ea649c637
Daniel PC <daniel.barczynski@onet.pl>
2022-10-14 01:35:32 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿TowerOfHanoi.Sort(4, 'a', 'b', 'c');
+
+
+public class TowerOfHanoi
+{
+    public static void Sort(int num, char a = 'A', char b = 'B', char c ='C')
+    {
+        if (num == 1)
+            Console.WriteLine($"moving {a} to {c}");
+        
+        else
+        {
+            Sort(num - 1, a, c, b);
+            Console.WriteLine($"moving {a} to {c}");
+            Sort(num - 1, b, a, c);
+        }
+    }
+}
