bd8b4a4a14e7e9fc421142b4bc712d11700c74d9
Daniel PC <daniel.barczynski@onet.pl>
2022-10-18 18:53:59 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int knapsackSize = random.Next(100, 200);
+List<Knapsack> knapsackItems = new List<Knapsack>();
+
+for (int i = 0; i < 20; i++)
+{
+    knapsackItems.Add(new Knapsack(random.Next(1, 30), random.Next(1, 30)));
+}
+
+Console.WriteLine("S   V   P");
+
+foreach (var item in knapsackItems)
+{
+    Console.WriteLine(item.Size + " | " + item.Value + " | " + item.Profitability);
+}
+
+public class Knapsack
+{
+    public double Size { get; set; }
+    public double Value { get; set; }
+    public double Profitability { get; private set; }
+
+    public Knapsack(double size, double value)
+    {
+        Size = size;
+        Value = value;
+        Profitability = Math.Round(value/size, 2);
+    }
+}
