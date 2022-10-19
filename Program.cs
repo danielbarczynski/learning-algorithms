d7133b1df7cf7547a6f22bb0617ebec96998f6fe
Daniel PC <daniel.barczynski@onet.pl>
2022-10-19 14:35:02 +0200

    update

--- Program.cs
+++ Program.cs
@@ -3,8 +3,10 @@
     new Knapsack(3, 2), new Knapsack(1, 2), new Knapsack(3, 4), new Knapsack(4, 5), new Knapsack(2, 3)
 };
 
+int countLines = 0;
 int knapsackSize = 7;
 int itemsLength = items.Count();
+int[,] dynamicArray = new int[itemsLength, knapsackSize];
 
 Console.WriteLine("\nItems: ");
 
@@ -15,37 +17,42 @@ foreach (var item in items)
 
 for (int i = 0; i <= itemsLength - 1; i++)
 {
-    Console.WriteLine();
-// create arr[i, j] for storing ?
     for (int j = 0; j <= knapsackSize - 1; j++)
     {
-        
         if (items[i].Size <= i && i > 1 && i - 1 > 0 && j != 0)
         {
             if (items[i].Size + items[i - 1].Size <= j)
-                Console.Write(items[i].Value + items[i - 1].Value);
+                dynamicArray[i, j] = items[i].Value + items[i - 1].Value;
         }
 
         else if (items[i].Size <= i && j != 0)
         {
-            Console.Write(items[i].Value);
+            dynamicArray[i, j] = items[i].Value;
         }
 
         else
         {
             if (i - 1 > 0 && j != 0)
-                Console.Write(items[i - 1].Value);
+                dynamicArray[i, j] = items[i - 1].Value;
             else
-                Console.Write(0);
+                dynamicArray[i, j] = 0;
         }
     }
 }
 
+foreach (var item in dynamicArray)
+{
+    if (countLines % 7 == 0)
+        Console.WriteLine();
+
+    countLines++;
+    Console.Write(item + " ");
+}
 
 public class Knapsack
 {
-    public double Size { get; set; }
-    public double Value { get; set; }
+    public int Size { get; set; }
+    public int Value { get; set; }
     private Knapsack[] arr = new Knapsack[7];
 
     public Knapsack this[int i]
@@ -54,7 +61,7 @@ public class Knapsack
         set { arr[i] = value; }
     }
 
-    public Knapsack(double size, double value)
+    public Knapsack(int size, int value)
     {
         Size = size;
         Value = value;
