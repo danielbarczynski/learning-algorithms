91d3faa77292fe983f3de521c987d1e8de0d2fb6
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-10-20 20:28:52 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
 ﻿Random random = new Random();
 int[] numbers = new int[random.Next(5, 30)];
 int targetNumber = random.Next(1, 100);
-
+int leftIndex = 0;
+int rightIndex = numbers.Length - 1;
 for (int i = 0; i < numbers.Length - 1; i++)
 {
     numbers[i] = random.Next(1, 100);
 }
 
 int[] sortedNumbers = numbers.OrderBy(x => x).ToArray();
-BinarySearch(targetNumber, sortedNumbers);
+BinarySearch(sortedNumbers, targetNumber, leftIndex, rightIndex);
 
-static int BinarySearch(int targetNumber, int[] numbers)
+static int BinarySearch(int[] numbers, int targetNumber, int leftIndex, int rightIndex)
 {
     int middleNumber = (numbers[0] + numbers[numbers.Length - 1]) / 2;
     for (int i = 0; i < numbers.Length - 1; i++)
@@ -19,7 +20,10 @@ static int BinarySearch(int targetNumber, int[] numbers)
             return middleNumber;
         
         else if (middleNumber > targetNumber)
-            BinarySearch(targetNumber, numbers);
+            BinarySearch(numbers, targetNumber, 0, middleNumber--);
+        
+        else
+            BinarySearch(numbers, targetNumber, middleNumber++, rightIndex);
 
     }
 
