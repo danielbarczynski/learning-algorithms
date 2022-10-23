85a04d143155cd752dca4602141800287e939e0f
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-10-23 14:04:48 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write(Fibonacci(4));
+Console.Write(FibonacciRecursive(4));
+
+int Fibonacci(int n)
+{
+    if (n <= 2)
+        return 1;
+    
+    int result = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        result = i + (i - 1);
+    }
+
+    return result;
+}
+
+int FibonacciRecursive(int n)
+{
+    return n;
+}
