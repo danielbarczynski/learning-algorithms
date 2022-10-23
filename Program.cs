40c7283abe660b9348e60c90a1777d5bd472868d
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-10-23 20:25:12 +0200

    cache done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var cache = new Dictionary<int, int>();
+
+Console.WriteLine(Square(300));
+
+foreach (var item in cache)
+{
+    Console.WriteLine("dictionary items: " + item.Key + " " + item.Value);
+}
+
+Console.WriteLine(Square(300));
+Console.WriteLine(Square(300));
+Console.WriteLine(Square(300));
+Console.WriteLine(Square(300));
+Console.WriteLine(Square(300));
+
+int Square(int n)
+{
+    int result = 0;
+
+    if (cache.ContainsKey(n))
+    {
+        Console.Write("cache ");
+        return cache[n];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result++;
+        }
+    }
+
+    cache.Add(n, result);
+
+    return result;
+}
