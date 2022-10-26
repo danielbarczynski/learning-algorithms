00cec2cca36021e646940db207f04bd8cfff4bfa
Daniel PC <daniel.barczynski@onet.pl>
2022-10-27 00:44:12 +0200

    sum done

--- Classes/SimpleTree.cs
+++ Classes/SimpleTree.cs
@@ -1,22 +1,5 @@
 public class SimpleTree
 {
     public int key;
-    public SimpleTree  left, right;
-
-    public static SimpleTree AddNode(int key)
-    {
-        SimpleTree simpleTree = new SimpleTree();
-        simpleTree.key = key;
-        simpleTree.left = simpleTree.right = null;
-
-        return (simpleTree);
-    }
-
-    public static int Sum(SimpleTree root)
-    {
-        if (root == null)
-            return 0;
-
-        return (root.key + Sum(root.left) + Sum(root.right));
-    }
+    public SimpleTree left, right;
 }
--- Program.cs
+++ Program.cs
@@ -1,16 +1,45 @@
 ﻿using Tree;
 
-var tree = new BinaryTree();
+// var tree = new BinaryTree();
 
-tree.Insert(10);
-tree.Insert(3);
-tree.Insert(2);
-tree.Insert(4);
-tree.Insert(18);
-tree.Insert(7);
-tree.Insert(22);
-tree.Insert(20);
+// tree.Insert(10);
+// tree.Insert(3);
+// tree.Insert(2);
+// tree.Insert(4);
+// tree.Insert(18);
+// tree.Insert(7);
+// tree.Insert(22);
+// tree.Insert(20);
 
-tree.Traverse(); 
+// tree.Traverse(); 
 
-Console.WriteLine();
+// Console.WriteLine();
+
+    SimpleTree AddNode(int key)
+    {
+        SimpleTree simpleTree = new SimpleTree();
+        simpleTree.key = key;
+        simpleTree.left = simpleTree.right = null;
+
+        return (simpleTree);
+    }
+
+    static int Sum(SimpleTree root)
+    {
+        if (root == null)
+            return 0;
+
+        return (root.key + Sum(root.left) + Sum(root.right));
+    }
+    
+    SimpleTree root = AddNode(1);
+    root.left = AddNode(2);
+    root.right = AddNode(3);
+    root.left.left = AddNode(4);
+    root.left.right = AddNode(5);
+    root.right.left = AddNode(6);
+    root.right.right = AddNode(7);
+    root.right.left.right = AddNode(8);
+ 
+    int sum = Sum(root);
+    Console.WriteLine("Sum of all the elements is: " + sum);
