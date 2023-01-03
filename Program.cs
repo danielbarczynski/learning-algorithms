e0e009eee4c54c8589d0bb4465f321d74b5bf81b
Daniel Barczyński <daniel.barczynski@onet.pl>
2023-01-03 18:31:23 +0100

    up

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-﻿int[] value = { 2, 2, 4, 5, 3 };
-int[] weight = { 3, 1, 3, 4, 2 };
+﻿int[] values = { 2, 2, 4, 5, 3 };
+int[] weights = { 3, 1, 3, 4, 2 };
 int capacity = 7;
-int itemsCount = value.Length;
+int itemsCount = values.Length;
 
-if (value.Length == weight.Length)
+if (values.Length == weights.Length)
 {
-    int result = Knapsack(capacity, weight, value, itemsCount);
+    int result = Knapsack(capacity, weights, values, itemsCount);
 	Console.WriteLine("\n");
     Console.WriteLine(result);
 }
 
-static int Knapsack(int capacity, int[] weight, int[] value, int itemsCount)
+static int Knapsack(int capacity, int[] weights, int[] values, int itemsCount)
 {
     int[,] knapsack = new int[itemsCount + 1, capacity + 1];
 
@@ -26,20 +26,20 @@ static int Knapsack(int capacity, int[] weight, int[] value, int itemsCount)
 				Console.Write(knapsack[i, j] + " ");
 			}
 
-			else if (weight[i - 1] <= j)
+			else if (weights[i - 1] <= j)
 			{
-				// current value + diagonal arrow value or vertical arrow
+				// current values + diagonal arrow values or vertical arrow
 				// for i = 2, j = 4 score = 4
 				// [2 + [1, 4 - 1] = 2, 2]
 				// Max(4, 2) = 4  
-				// weight[i - 1] - cofamy sie o tyle, ile wynosi waga j =5, waga 3, ponieważ wtedy wartość się zgadzała
-                knapsack[i, j] = Math.Max(value[i - 1] + knapsack[i - 1, j - weight[i - 1]], knapsack[i - 1, j]); 
+				// weights[i - 1] - cofamy sie o tyle, ile wynosi waga j =5, waga 3, ponieważ wtedy wartość się zgadzała
+                knapsack[i, j] = Math.Max(values[i - 1] + knapsack[i - 1, j - weights[i - 1]], knapsack[i - 1, j]); 
 				Console.Write(knapsack[i, j] + " ");
 			}
 
 			else
             {
-				// value from vertical arrow
+				// values from vertical arrow
 			    knapsack[i, j] = knapsack[i - 1, j]; 
 				Console.Write(knapsack[i, j] + " ");
 			}
