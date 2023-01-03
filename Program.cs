d117301d12f0418078a6ea3f4de671c24c60527f
Daniel Barczyński <daniel.barczynski@onet.pl>
2023-01-03 19:01:27 +0100

    up

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿FibonacciIterative(9);
-Console.WriteLine(FibonacciRecursive(9));
+Console.WriteLine(FibonacciRecursive(4));
 
 void FibonacciIterative(int n)
 {
@@ -25,6 +25,11 @@ int FibonacciRecursive(int n)
 {
     if (n <= 2)
         return 1;
-    
+    // for n = 4: 
+	// called once on n4 for n - 1 (3) 
+	// called once on n3 for n - 1 (2) returns 1
+	// called once on n3 for n - 2 (1) returns 1
+	// backing - called once on n4 for n - 2 (2) returns 1. result = 3
+	// for n =5: this will be called 3 times for n -1 and 2 times for n - 2. result = 5
     return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
 }
